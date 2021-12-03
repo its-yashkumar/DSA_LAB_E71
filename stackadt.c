95e9c3803611effad91f644d17200b312ca3284d
Yashkumar Jain <jainys_3@rknec.edu>
2021-12-03 09:56:02 +0530

    wriiten isfull function to see whether stack is full or not

--- stackadt.c
+++ stackadt.c
@@ -6,13 +6,22 @@ struct Mystack{
  int* sptr;
 };
 void createstack(struct Mystack *);
+int isfull(struct Mystack *);
+void push(struct Mystack*,int);
 int main(){
    struct Mystack stack;
    printf("Enter the value of size of stack :\n");
    scanf("%d",&stack.size);
    createstack(&stack);
+
 }
 void createstack(struct Mystack* stack){
     stack->top=-1;
     stack->sptr=(int *)calloc(stack->size,sizeof(struct Mystack));
+}
+int isfull(struct Mystack * stack){
+    if(stack->top==stack->size-1){
+        return 1;
+    }
+    else return 0;
 }
