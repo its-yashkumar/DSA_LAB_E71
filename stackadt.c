f9cf3f9c8c5e836e06b092bd5e71ac29ea989aeb
Yashkumar Jain <jainys_3@rknec.edu>
2021-12-03 09:29:26 +0530

    created structure variable and taken input of size from user

--- stackadt.c
+++ stackadt.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
+struct Mystack{
+ int top;
+ int size;
+ int* sptr;
+};
+void createstack(struct Mystack *,int);
 int main(){
-    
+   struct Mystack stack;
+   int n;
+   printf("Enter the value of size of stack :\n");
+   scanf("%d",&n);
 }
