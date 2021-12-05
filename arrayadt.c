3e7bc0307ce21556d68bfabc8f3bf7092e38af26
Yashkumar <jainy6214@gmail.com>
2021-12-05 09:22:46 +0530

    get the elements of array using function getarray

--- arrayadt.c
+++ arrayadt.c
@@ -6,6 +6,7 @@ struct Arrayadt{
   int * aptr;
 };
 void createarray(struct Arrayadt *);
+void getarray(struct Arrayadt *);
 int main(){
   struct Arrayadt arr;
   printf("Enter the capacity of the array \n");
@@ -13,8 +14,15 @@ int main(){
   printf("Enter the size of the array \n");
   scanf("%d",&arr.size);
   createarray(&arr);
+  getarray(&arr);
 
 }
 void createarray (struct Arrayadt * arr){
    arr->aptr=(int *)calloc(arr->capacity,sizeof(int));
+}
+void getarray(struct Arrayadt * arr){
+   for(int i=0;i<arr->size;i++){
+     printf("Enter %d element of array : \n",i);
+     scanf("%d",&(arr->aptr)[i]);
+   }
 }
