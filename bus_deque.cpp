cf566874721e8b2094ddf99012e138b17c21ace2
Yashkumar <jainy6214@gmail.com>
2021-12-17 10:20:59 +0530

    created enqueue_rear

--- bus_deque.cpp
+++ bus_deque.cpp
@@ -13,10 +13,13 @@ int getfront(struct Dequeue *);
 int getrear(struct Dequeue *);
 int isoverflow(struct Dequeue *);
 int isunderflow(struct Dequeue *);
+void enqueue_rear(struct Dequeue *);
 
 int main(){
    struct Dequeue deq;
    createdequeue(&deq);
+   cout<<"Enter Commands to perform following operations"<<endl;
+
 
 }
 void createdequeue(struct Dequeue * deq){
@@ -26,9 +29,11 @@ void createdequeue(struct Dequeue * deq){
      deq->dptr=(int *)calloc(deq->capacity,sizeof(int));
 }
 int getfront(struct Dequeue * deq){
+     cout<<(deq->dptr)[deq->front];
      return (deq->dptr)[deq->front];
 }
 int getrear(struct Dequeue * deq){
+     cout<<(deq->dptr)[deq->rear];
      return (deq->dptr)[deq->rear];
 }
 int isoverflow(struct Dequeue * deq){
@@ -42,4 +47,21 @@ int isunderflow(struct Dequeue * deq){
         return 1;
     }
     else return 0;
+}
+void enqueue_rear(struct Dequeue * deq){
+    if(isoverflow(deq)){
+        cout<<"The queue is full cannot add further elements"<<endl;
+    }
+    else if(isunderflow(deq)){
+        deq->front=deq->rear=0;
+        (deq->dptr)[deq->rear]=1;
+    }
+    else if(deq->rear==deq->capacity-1){
+        deq->rear=0;
+        (deq->dptr)[deq->rear]=1;
+    }
+    else{
+        deq->rear++;
+        (deq->dptr)[deq->rear]=1;
+    }
 }
