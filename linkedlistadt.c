53cc1e885bab58036abbf43f3ae34ae5c6a8c550
Yashkumar <jainy6214@gmail.com>
2021-12-24 09:36:00 +0530

    created a head of linked list and entered data in it

--- linkedlistadt.c
+++ linkedlistadt.c
@@ -4,11 +4,20 @@ struct Node{
   int data;
   struct Node * next;
 };
-struct Node * head,
+struct Node * head;
+
 void createlinkedlist();
+
 int main(){
-    
+    struct Node * ptr;
+    createlinkedlist();
+    ptr=head;
+    printf("%d",head->data);
 }
 void createlinkedlist(){
+  int data;
     head=(struct Node *)malloc(sizeof(struct Node));
+    printf("Enter data you want to insert\n");
+    scanf("%d",&data);
+    head->data=data;
 }
