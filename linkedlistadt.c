5bb6efa75f816820b6f29fec3b973244296778b2
Yashkumar <jainy6214@gmail.com>
2021-12-24 10:28:54 +0530

    written code to insert at begin and end

--- linkedlistadt.c
+++ linkedlistadt.c
@@ -4,20 +4,68 @@ struct Node{
   int data;
   struct Node * next;
 };
-struct Node * head;
 
-void createlinkedlist();
+
+struct Node * createlinkedlist(struct Node *);
+void display(struct Node *);
+struct Node * insertatbegin(struct Node *);
+struct Node * insertatend(struct Node *);
+struct Node * insertatindex(struct Node *,int index);
 
 int main(){
-    struct Node * ptr;
-    createlinkedlist();
-    ptr=head;
-    printf("%d",head->data);
+    struct Node * head;
+    head=createlinkedlist(head);
+    display(head);
+    head=insertatbegin(head);
+    printf("After insert at begin\n");
+    display(head);
+    head=insertatend(head);
+    printf("After insert at end\n");
+    display(head);
 }
-void createlinkedlist(){
-  int data;
+struct Node * createlinkedlist(struct Node * head){
+    int data;
     head=(struct Node *)malloc(sizeof(struct Node));
     printf("Enter data you want to insert\n");
     scanf("%d",&data);
     head->data=data;
+    head->next=NULL;
+    return head;
+}
+void display(struct Node * head){
+    struct Node* ptr;
+    ptr=head;
+    while(ptr != NULL){
+       printf("%d\n",ptr->data);
+       ptr=ptr->next;
+    }
+}
+struct Node * insertatbegin(struct Node *head){
+    struct Node * ptr = (struct Node *) malloc(sizeof(struct Node));
+    int data;
+    printf("Enter data you want to insert at begin\n");
+    scanf("%d",&data);
+
+    ptr->data = data;
+
+    ptr->next = head;
+    return ptr;
+}
+struct Node * insertatend(struct Node *head){
+    struct Node * ptr,*p;
+    p=head;
+    ptr = (struct Node *) malloc(sizeof(struct Node));
+    int data;
+    printf("Enter data you want to insert at end\n");
+    scanf("%d",&data);
+    ptr->data = data;
+    while(p->next != NULL){
+        p=p->next;
+    }
+    p->next=ptr;
+    ptr->next=NULL;
+    return head;
+}
+struct Node * insertatend(struct Node *head,int index){
+    
 }
