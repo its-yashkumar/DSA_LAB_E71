f51e4c5be5abf3a75401e0ca239ecaf538ad02ed
Yashkumar <jainy6214@gmail.com>
2022-01-07 09:47:04 +0530

    complted info of creating polynomial

--- polylinkadd.c
+++ polylinkadd.c
@@ -9,11 +9,24 @@ struct Polynomial
 };
 
 struct Polynomial * createpolyequation(struct Polynomial* p);
+struct Polynomial * addterms(struct Polynomial* p);
+
 int main(){
     struct Polynomial* p1,*p2;
     p1=createpolyequation(p1);
+    p2=createpolyequation(p2);
+    printf("Enter terms of polynomials p1");
+    p1=addterms(p1);
+
 }
 struct Polynomial * createpolyequation(struct Polynomial* p){
     p=(struct Polynomial *)malloc(sizeof(struct Polynomial));
+    p->coeff=0
+    p->expo=0;
+    p->next=NULL;
+
     return p;
+}
+struct Polynomial * addterms(struct Polynomial* p){
+
 }
