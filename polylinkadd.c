7aab375ee5628ae5f1a7d904a49a9e3ccab13c16
Yashkumar <jainy6214@gmail.com>
2022-01-07 09:42:35 +0530

    created a polynomial

--- polylinkadd.c
+++ polylinkadd.c
@@ -1,8 +1,19 @@
 //Add two polynomials using linked list
+#include<stdio.h>
+#include<stdlib.h>
 struct Polynomial
 {
-    int coeff;
-    int expo;
+    float coeff;
+    float expo;
     struct Polynomial* next;
 };
 
+struct Polynomial * createpolyequation(struct Polynomial* p);
+int main(){
+    struct Polynomial* p1,*p2;
+    p1=createpolyequation(p1);
+}
+struct Polynomial * createpolyequation(struct Polynomial* p){
+    p=(struct Polynomial *)malloc(sizeof(struct Polynomial));
+    return p;
+}
