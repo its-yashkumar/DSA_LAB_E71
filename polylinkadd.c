8b17c2025ee6e278ba219b99374c6968951c331e
Yashkumar <jainy6214@gmail.com>
2022-01-18 17:20:05 +0530

    wriitten program to add polynomials

--- polylinkadd.c
+++ polylinkadd.c
@@ -10,9 +10,9 @@ struct Polynomial
 
 struct Polynomial * createpolyequation(struct Polynomial* p,int);
 struct Polynomial * addterms(struct Polynomial* p,int,int);
-
+void polyadd(struct Polynomial*,struct Polynomial * , struct Polynomial *);
 int main(){
-    struct Polynomial* p1,*p2;
+    struct Polynomial* p1,*p2,*p3;
     int count1=0,count2=0,expo;
     int coeff,com;
     while(1){
@@ -49,8 +49,10 @@ int main(){
       printf("Enter -1 to exit or press any key to continue");
       scanf("%d",&com);
       if(com==-1)break;
-   
-    }
+   }
+  //Performing addition
+   polyadd(p1,p2,p3);
+}
 }
 struct Polynomial * createpolyequation(struct Polynomial* p,int coeff){
     p=(struct Polynomial *)malloc(sizeof(struct Polynomial));
@@ -66,6 +68,57 @@ struct Polynomial * addterms(struct Polynomial* p,int coeff,int count1){
     pterm->expo=count1;
     pterm->next=p;
     return pterm;
+}
 
+void polyadd(struct Polynomial* poly1, struct Polynomial* poly2,
+             struct Polynomial* poly)
+{
+    while (poly1->next && poly2->next) {
+        // If power of 1st polynomial is greater then 2nd,
+        // then store 1st as it is and move its pointer
+        if (poly1->expo > poly2->expo) {
+            poly->expo = poly1->expo;
+            poly->coeff = poly1->coeff;
+            poly1 = poly1->next;
+        }
 
+        // If power of 2nd polynomial is greater then 1st,
+        // then store 2nd as it is and move its pointer
+        else if (poly1->expo < poly2->expo) {
+            poly->expo = poly2->expo;
+            poly->coeff = poly2->coeff;
+            poly2 = poly2->next;
+        }
+
+        // If power of both polynomial numbers is same then
+        // add their coefficients
+        else {
+            poly->expo= poly1->expo;
+            poly->coeff = poly1->coeff + poly2->coeff;
+            poly1 = poly1->next;
+            poly2 = poly2->next;
+        }
+
+        // Dynamically create new node
+        poly->next
+            = (struct Polynomial*)malloc(sizeof(struct Polynomial));
+        poly = poly->next;
+        poly->next = NULL;
+    }
+    while (poly1->next || poly2->next) {
+        if (poly1->next) {
+            poly->expo = poly1->expo;
+            poly->coeff = poly1->coeff;
+            poly1 = poly1->next;
+        }
+        if (poly2->next) {
+            poly->expo = poly2->expo;
+            poly->coeff = poly2->coeff;
+            poly2 = poly2->next;
+        }
+        poly->next
+            = (struct Polynomial*)malloc(sizeof(struct Polynomial));
+        poly = poly->next;
+        poly->next = NULL;
+    }
 }
