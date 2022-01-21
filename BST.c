2abafc9b4da5f23b9e1575181f0f2b158dd1b25d
Yashkumar <jainy6214@gmail.com>
2022-01-21 14:36:56 +0530

    added function to insert node in BST

--- BST.c
+++ BST.c
@@ -6,10 +6,28 @@ struct Node
    struct Node * left;
    struct Node * right;
 };
-struct Node* create(int data){
-    
-}
-
+struct Node* create(int data);
+struct Node* insert(int data);
 int main(){
     
+}
+struct Node* create(int data){
+   struct Node * tree;
+   tree=(struct Node *)malloc(sizeof(struct Node));
+   tree->data=data;
+   tree->left=NULL;
+   tree->right=NULL;
+   return tree;
+}
+struct Node* insert(struct Node *root,int data){
+   if(root==NULL){
+       return create(data);
+   }
+   else if(data>root->data){
+       root->right=insert(root->right,data);
+   }
+   else{
+       root->left=insert(root->left,data);
+   }
+   return root;
 }
