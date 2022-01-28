d617bee99cbf2ddd2099eb9ab2602f7743d75c7f
Yashkumar <jainy6214@gmail.com>
2022-01-28 10:50:34 +0530

    entered data in doubly linked list

--- doublylinkedlist.c
+++ doublylinkedlist.c
@@ -5,8 +5,15 @@ struct Node
     int data;
     struct Node * next,*prev;
 };
+struct Node * head,*tail;
+void create(int data){
+    struct Node * ptr;
+    ptr=(struct Node *)malloc(sizeof(struct Node));
+    ptr->data=data;
+    head=ptr;
+    tail=ptr;
+}
 
 int main(){
-   struct Node * head,*tail;
-   
+  
 }
