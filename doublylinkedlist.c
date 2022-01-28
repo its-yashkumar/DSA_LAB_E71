1963f464c55c35913b4d6c467df6d4f981b36e04
Yashkumar <jainy6214@gmail.com>
2022-01-28 13:22:47 +0530

    assigned NULL in pointers of head and tail. Written a function to display from head of linked list

--- doublylinkedlist.c
+++ doublylinkedlist.c
@@ -6,14 +6,27 @@ struct Node
     struct Node * next,*prev;
 };
 struct Node * head,*tail;
+void create(int data);
+void displaybegin();
+
+int main(){
+   create(34);
+   displaybegin();
+}
 void create(int data){
     struct Node * ptr;
     ptr=(struct Node *)malloc(sizeof(struct Node));
     ptr->data=data;
+    ptr->next=NULL;
+    ptr->prev=NULL;
     head=ptr;
     tail=ptr;
 }
-
-int main(){
-  
+void displaybegin(){
+    struct Node* ptr;
+    ptr=head;
+    while(ptr != NULL){
+       printf("%d\n",ptr->data);
+       ptr=ptr->next;
+}
 }
