46b2c71097510aad7c09e8e8e7bbdb4c0e04bdd0
Yashkumar <jainy6214@gmail.com>
2022-01-30 14:27:05 +0530

    created a function to insert at begin of linked list and to traverse from tail

--- doublylinkedlist.c
+++ doublylinkedlist.c
@@ -8,10 +8,16 @@ struct Node
 struct Node * head,*tail;
 void create(int data);
 void displayhead();
+void displaytail();
+void insertatbegin();
 
 int main(){
    create(34);
+   insertatbegin();
+   printf("Traversing from head\n");
    displayhead();
+   printf("Traversing from tail\n");
+   displaytail();
 }
 void create(int data){
     struct Node * ptr;
@@ -32,6 +38,31 @@ void displayhead(){
     while(ptr != NULL){
        printf("%d\n",ptr->data);
        ptr=ptr->next;
-}
+       }
     }
+}
+void displaytail(){
+    struct Node* ptr;
+    ptr=tail;
+    if(tail==NULL){
+        printf("List is empty");
+    }
+    else{
+    while(ptr != NULL){
+       printf("%d\n",ptr->data);
+       ptr=ptr->prev;
+       }
+    }
+}
+void insertatbegin(){
+    struct Node * ptr = (struct Node *) malloc(sizeof(struct Node));
+    int data;
+    printf("Enter data you want to insert at begin\n");
+    scanf("%d",&data);
+
+    ptr->data = data;
+    ptr->next = head;
+    ptr->prev=NULL;
+    head->prev=ptr;
+    head=ptr;
 }
