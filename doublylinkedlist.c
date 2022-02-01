f96e52d8f2997610c243c177971d049de7eacf6c
Yashkumar <jainy6214@gmail.com>
2022-02-01 11:49:54 +0530

    created function to delete at begin

--- doublylinkedlist.c
+++ doublylinkedlist.c
@@ -21,7 +21,8 @@ int main(){
   while(1){
   printf("Enter commands to perform following operations :\n");
   printf("1)To create doubly linked list\n2)To traverse from head\n3)To traverse from tail\n");
-  printf("4)To insert at begin of list\n5)To insert at end\n6)To insert at index-1)To exit\n");
+  printf("4)To insert at begin of list\n5)To insert at end\n6)To insert at index\n");
+  printf("7)To delete at begin-1)To exit\n");
   scanf("%d",&com);
   if(com==1){
       int data;
@@ -51,7 +52,10 @@ int main(){
   {
       insertatindex();
   }
-  
+  else if (com==7)
+  {
+      deleteatbegin();
+  }
   else if (com==-1)
   {
       return 0;
@@ -75,7 +79,7 @@ void displayhead(){
     struct Node* ptr;
     ptr=head;
     if(head==NULL){
-        printf("List is empty");
+        printf("List is empty\n");
     }
     else{
     while(ptr != NULL){
@@ -88,7 +92,7 @@ void displaytail(){
     struct Node* ptr;
     ptr=tail;
     if(tail==NULL){
-        printf("List is empty");
+        printf("List is empty\n");
     }
     else{
     while(ptr != NULL){
@@ -141,9 +145,7 @@ void insertatindex(){
     }
     else{
        {  
-       temp=head;  
-    //    printf("Enter the location");  
-    //    scanf("%d",&position);  
+       temp=head;   
        for(i=0;i<position;i++)  
        {  
            temp = temp->next;  
@@ -163,6 +165,27 @@ void insertatindex(){
        printf("Node inserted\n");  
    }  
 }  
+}
+void deleteatbegin(){
+    struct Node *ptr;  
+    if(head == NULL)  
+    {  
+        printf("List is Empty\n");  
+    }  
+    else if(head->next == NULL)  
+    {  
+        head = NULL;   
+        free(head);  
+        printf("Node deleted\n");  
+    }  
+    else  
+    {  
+        ptr = head;  
+        head = head -> next;  
+        head -> prev = NULL;  
+        free(ptr);  
+        printf("Node deleted\n");  
+    }  
 }
     
 
