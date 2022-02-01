93b6b14500f55ae468a14ca3c92ce97f51b504b4
Yashkumar <jainy6214@gmail.com>
2022-02-01 14:03:32 +0530

    inserted brackets in printf

--- doublylinkedlist.c
+++ doublylinkedlist.c
@@ -19,10 +19,12 @@ void deleteatindex();
 int main(){
   int com;
   while(1){
+  {
   printf("Enter commands to perform following operations :\n");
   printf("1)To create doubly linked list\n2)To traverse from head\n3)To traverse from tail\n");
   printf("4)To insert at begin of list\n5)To insert at end\n6)To insert at index\n");
   printf("7)To delete at begin\n8)To delete at end\n-1)To exit\n");
+  }
   scanf("%d",&com);
   if(com==1){
       int data;
